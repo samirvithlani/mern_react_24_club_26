57cefa0905f423f0db9515f29c9958691df0e87d
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-24 16:46:48 +0530

    conditinal styling

--- src/components/MapDemo3.jsx
+++ src/components/MapDemo3.jsx
@@ -5,7 +5,7 @@ export const MapDemo3 = () => {
     var employees = [
         {id:101,name:"amit",age:23,gender:"male",salary:23000},
         {id:102,name:"amita",age:26,gender:"female",salary:49000},
-        {id:103,name:"neha",age:28,gender:"female",salary:51000},
+        {id:103,name:"neha",age:51,gender:"female",salary:51000},
         {id:104,name:"jay",age:32,gender:"male",salary:78000},
         {id:105,name:"priya",age:27,gender:"female",salary:45000},
 
@@ -27,12 +27,13 @@ export const MapDemo3 = () => {
             <tbody>
                     {
                         employees.map((emp)=>{
-                            return <tr>
+                            return <tr style={{backgroundColor:emp.gender=="female" && "lightpink"}}>
                                 <td>{emp.id}</td>
                                 <td>{emp.name}</td>
-                                <td>{emp.age}</td>
+                                <td style={{color:emp.age>30 ?"red":"white"}}>{emp.age} {emp.age>50 && "*"} </td>
                                 <td>{emp.gender}</td>
-                                <td>{emp.salary}</td>
+                                {/* <td style={{backgroundColor:emp.salary>50000 ?"yellow" :"grey"}}>{emp.salary}</td> */}
+                                <td style={{backgroundColor:emp.salary>50000 &&"yellow" }}>{emp.salary}</td>
                             </tr>
                         })
                     }
