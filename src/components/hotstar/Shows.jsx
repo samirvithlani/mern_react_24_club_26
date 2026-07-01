66d0249966d728631365bef8eee897361a8ab20d
samirvithlani <samir.vithlani83955@gmail.com>
2026-07-01 17:16:03 +0530

    dynamic routing

--- src/components/hotstar/Shows.jsx
+++ src/components/hotstar/Shows.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Shows = () => {
+
+  const showList = [
+    {id:101,name:"BREAKING BAD"},
+    {id:102,name:"MONEY HEIST"},
+    {id:103,name:"STRANGER THINGS"},
+  ]
   return (
     <div style={{textAlign:"center"}}>
         <h1>Shows</h1>
+        {
+          showList.map((show)=>{
+              return <li> <Link to={`/watch/${show.name}`}>{show.name}</Link></li>
+          })
+        }
     </div>
   )
 }
